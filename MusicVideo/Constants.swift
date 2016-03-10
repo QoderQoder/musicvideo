01544bf4d84aef891594c5c5d91f4e707b22b34c
JR Spencer <jr.spencer@outlook.com>
2016-03-10 15:07:49 -0600

    Part 4 completed

---
+++ MusicVideo/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MusicVideo
+//
+//  Created by Agent86 on 3/10/16.
+//  Copyright © 2016 Agent86. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
+
